2a34129c2a64a364315cc93587ab413c2a22f000
DaHub49 <dyls.martin.49@gmail.com>
2024-12-26 08:42:41 +0200

    26/12/24 08:42: The Complete Android 14 & Kotlin Development Masterclass: Video 144
    -> Added the data class Category with 4 Strings as parameters.
    -> Added the data class CategoriesResponse with a List.

---
+++ app/src/main/java/za/co/howtogeek/myrecipeapp/Category.kt
@@ -0,0 +1,22 @@
+package za.co.howtogeek.myrecipeapp
+
+/*
+    JSON data from https://themealdb.com/api.php:
+      "idCategory": "1",
+      "strCategory": "Beef",
+      "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
+      "strCategoryDescription"
+ */
+
+// data class on the different categories:
+data class Category(
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+    )
+
+
+data class CategoriesResponse(
+    val categories: List<Category>
+)
