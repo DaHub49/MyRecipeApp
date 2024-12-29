15791ab4a128b4d73650b2973116f1d3f5a45067
DaHub49 <dyls.martin.49@gmail.com>
2024-12-29 06:48:05 +0200

    29/12/24 06:47: The Complete Android 14 & Kotlin Development Masterclass: Video 149
    -> Added @Composable CategoryScreen
    -> Added @Composable CategoryItem
    -> Edited MainActivity to display RecipeScreen()
    -> Still need to add internet permissions to AndroidManifest

--- app/src/main/java/za/co/howtogeek/myrecipeapp/MainActivity.kt
+++ app/src/main/java/za/co/howtogeek/myrecipeapp/MainActivity.kt
@@ -3,45 +3,25 @@ package za.co.howtogeek.myrecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import za.co.howtogeek.myrecipeapp.ui.theme.MyRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyRecipeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Surface container using background colour:
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background)
+                {
+                    RecipeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyRecipeAppTheme {
-        Greeting("Android")
     }
 }
